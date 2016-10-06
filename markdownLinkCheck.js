8f9388d9938f7c368a3269b20032e7cc3ad4c876
Anike Arni <aarni@thoughtworks.com>
2016-10-06 11:39:07 -0300

    Arruma identação do script de markdown

--- markdownLinkCheck.js
+++ markdownLinkCheck.js
@@ -8,53 +8,53 @@ var baseUrl = process.argv[2];
 var markdown = '';
 
 var statusLabels = {
-	alive: "OK",
-	dead: "ERROR"
+  alive: "OK",
+  dead: "ERROR"
 };
 
 var isFailedRelativeUrl = function (result) {
-	return result.status === 'dead' && !result.link.includes('://');
+  return result.status === 'dead' && !result.link.includes('://');
 };
 
 var isNotRelativeUrl = function (result) {
-	return result.link.includes('://');
+  return result.link.includes('://');
 };
 
 var printResultsAndCountFailedUrls = function (results) {
-	return results.filter(isNotRelativeUrl)
-		.reduce(function (f, result) {
-			console.log('[%s] %s', statusLabels[result.status], result.link);
-			return (result.status === 'dead') ? f + 1 : f;
-		}, 0);
+  return results.filter(isNotRelativeUrl)
+    .reduce(function (f, result) {
+      console.log('[%s] %s', statusLabels[result.status], result.link);
+      return (result.status === 'dead') ? f + 1 : f;
+    }, 0);
 };
 
 var tryAgainWithRelativeUrls = function (results) {
-	results.filter(isFailedRelativeUrl)
-		.forEach(function (result) {
-			var completeMarkdownUrl = '[relative url](' + baseUrl + '' + result.link + ')';
-			checkMarkdown(completeMarkdownUrl, failTestIfBrokenLinks);
-		});
+  results.filter(isFailedRelativeUrl)
+    .forEach(function (result) {
+      var completeMarkdownUrl = '[relative url](' + baseUrl + '' + result.link + ')';
+      checkMarkdown(completeMarkdownUrl, failTestIfBrokenLinks);
+    });
 };
 
 var checkMarkdown = function (markdown, callback) {
-	markdownLinkCheck(markdown, function (err, results) {
-		totalFailedUrls += printResultsAndCountFailedUrls(results);
-		tryAgainWithRelativeUrls(results);
+  markdownLinkCheck(markdown, function (err, results) {
+    totalFailedUrls += printResultsAndCountFailedUrls(results);
+    tryAgainWithRelativeUrls(results);
 
-		if(callback) callback(totalFailedUrls);
-	});
+    if(callback) callback(totalFailedUrls);
+  });
 };
 
 var failTestIfBrokenLinks = function (finalNumberOfFailedUrls) {
-	if (finalNumberOfFailedUrls > 0) {
-		console.log('\nTEST FAILED');
-		console.log('Number of broken urls: ' + finalNumberOfFailedUrls);
-		process.exit(1);
-	}
+  if (finalNumberOfFailedUrls > 0) {
+    console.log('\nTEST FAILED');
+    console.log('Number of broken urls: ' + finalNumberOfFailedUrls);
+    process.exit(1);
+  }
 };
 
 stream.on('data', function (chunk) {
-	markdown += chunk.toString();
+  markdown += chunk.toString();
 }).on('end', function () {
-	checkMarkdown(markdown);
+  checkMarkdown(markdown);
 });
