9037d3ad6330d8bdfdfbb8c6db525247a44cc91c
Anike Arni <aarni@thoughtworks.com>
2016-10-07 11:58:13 -0300

    Arruma identação, mais uma vez

--- updateDate.sh
+++ updateDate.sh
@@ -5,10 +5,10 @@ END="</em></p>"
 
 ## Topics
 for file in ./_book/topics/*.html; do
-	filename=$(echo ${file##*/} | cut -f 1 -d '.')
-	date="$(git log -1 --date=iso --format="%ad" -- "topics/$filename.md")"
+  filename=$(echo ${file##*/} | cut -f 1 -d '.')
+  date="$(git log -1 --date=iso --format="%ad" -- "topics/$filename.md")"
 
-	sed -i bak -e "s,\($START\).*\($END\),\1Última modificação: $date\2," ${file}
+  sed -i bak -e "s,\($START\).*\($END\),\1Última modificação: $date\2," ${file}
 done
 
 ## README
